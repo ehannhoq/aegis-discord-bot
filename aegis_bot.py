d0ca982006dabf1553a670572468d14804c5f392
Ehan Haque <ehann.hoq@gmail.com>
2026-04-02 00:58:28 -0400

    [FEAT]: Added postgresql database at supabase [FEAT]: Added auto-action options

--- aegis_bot.py
+++ aegis_bot.py
@@ -1,15 +1,25 @@
-import io
 import datetime
+import io
+import json
 import os
 from collections import defaultdict
+from enum import Enum
 
+import asyncpg
 import discord
+import numpy as np
 from discord.ext import tasks
 from discord.ext import commands
 from discord import app_commands
-import numpy as np
-from PIL import Image
 from dotenv import load_dotenv
+from PIL import Image
+
+
+class ActionType(Enum):
+    NONE = 0
+    TIMEOUT = 1
+    KICK = 2
+
 
 load_dotenv()
 
@@ -19,14 +29,10 @@ intents.members = True
 intents.guilds = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+runtime_settings = defaultdict(dict)
 
 cached_messages = defaultdict(list)
 processing_users = set()
-
-LOG_CHANNEL = 967959760274088009
-CHANNEL_THRESHOLD = 2
-TIME_WINDOW = 5
-TIMEOUT_DURATION = 7
 
 
 def format_duration(days):
@@ -34,6 +40,49 @@ def format_duration(days):
     if hours < 24:
         return f'{hours:g} {"hour" if hours == 1 else "hours"}'
     return f'{days:g} {"day" if days == 1 else "days"}'
+
+
+async def initialize_db():
+    bot.db = await asyncpg.create_pool(os.getenv('DATABASE_URI'))
+    await bot.db.execute("""
+        CREATE TABLE IF NOT EXISTS settings (
+            guild_id        BIGINT  PRIMARY KEY,
+            log_channel     BIGINT,
+            action_type INTEGER DEFAULT 0,
+            timeout_dur     FLOAT   DEFAULT 7,
+            det_win         FLOAT   DEFAULT 1,
+            chan_thres      INTEGER DEFAULT 3
+        )
+    """)
+
+
+async def save_settings(guildID, loggingID, actionType, timeoutDuration, detectionWindow, channelThreshold):
+    await bot.db.execute("""
+        INSERT INTO settings (guild_id, log_channel, action_type timeout_dur, det_win, chan_thres)
+        VALUES ($1, $2, $3, $4, $5)
+        ON CONFLICT (guild_id) DO UPDATE SET
+            log_channel = EXCLUDED.log_channel,
+            action_type = EXCLUDED.action_type
+            timeout_dur = EXCLUDED.timeout_dur,
+            det_win = EXCLUDED.det_win,
+            chan_thres = EXCLUDED.chan_thres
+    """, guildID, loggingID, actionType, timeoutDuration, detectionWindow, channelThreshold)
+
+
+async def retrieve_settings(guildID):
+    row = await bot.db.fetchrow(
+        "SELECT log_channel, action_type, timeout_dur, det_win, chan_thres FROM settings WHERE guild_id = $1",
+        guildID
+    )
+    if row:
+        return {
+            "LOGGING_CHANNEL": row["log_channel"],
+            "ACTION_TYPE": row["action_type"],
+            "TIMEOUT_DURATION": row["timeout_dur"],
+            "DETECTION_WINDOW": row["det_win"],
+            "CHANNEL_THRESHOLD": row["chan_thres"]
+        }
+    return None
 
 
 async def handle_compromised_account(guildID, userID, channels_used):
@@ -49,21 +98,27 @@ async def handle_compromised_account(guildID, userID, channels_used):
 
     user: discord.Member = await bot.get_guild(guildID).fetch_member(userID)
     try:
-        await user.timeout(datetime.timedelta(days=TIMEOUT_DURATION))
+        actionType = runtime_settings[guildID]['ACTION_TYPE']
+
+        if actionType == ActionType.TIMEOUT:
+            await user.timeout(datetime.timedelta(days=runtime_settings[guildID]['TIMEOUT_DURATION']))
+
+        if actionType == ActionType.KICK:
+            await user.kick(reason="[Aegis] Account suspected for being compromised.")
     except:
         pass
 
     embeded: discord.Embed = discord.Embed(
         title='Compromised Account Detected',
-        description=f'**User:** {user.global_name}\n**Reason:** Sent {len(messages)} messages in {channels_used} channels within {TIME_WINDOW} seconds.\n**Action Taken:** User timed out for {format_duration(TIMEOUT_DURATION)}',
+        description=f'**User:** {user.global_name}\n**Reason:** Sent {len(messages)} messages in {channels_used} channels within {runtime_settings[guildID]['DETECTION_WINDOW']} seconds.\n**Action Taken:** User timed out for {format_duration(runtime_settings[guildID]['TIMEOUT_DURATION'])}',
         color=0xff0000,
         timestamp=datetime.datetime.now()
     )
-    await bot.get_channel(LOG_CHANNEL).send(embed=embeded)
+    await bot.get_channel(runtime_settings[guildID]['LOGGING_CHANNEL']).send(embed=embeded)
 
 
 @tasks.loop(seconds=1)
-async def check_cache():
+async def clear_cache():
     now = datetime.datetime.now(datetime.timezone.utc)
     keys = list(cached_messages.keys())
 
@@ -73,7 +128,7 @@ async def check_cache():
 
         cached_messages[key] = [
             msg for msg in cached_messages[key]
-            if (now - msg.created_at) <= datetime.timedelta(seconds=TIME_WINDOW)
+            if (now - msg.created_at) <= datetime.timedelta(seconds=runtime_settings[msg.guild.id]['DETECTION_WINDOW'])
         ]
 
         if not cached_messages[key]:
@@ -89,7 +144,7 @@ async def on_message(message: discord.Message):
     cached_messages[key].append(message)
 
     channels_used = set(msg.channel for msg in cached_messages[key])
-    if len(channels_used) >= CHANNEL_THRESHOLD and key not in processing_users:
+    if len(channels_used) >= runtime_settings[message.guild.id]['CHANNEL_THRESHOLD'] and key not in processing_users:
         processing_users.add(key)
         try:
             await handle_compromised_account(*key, len(channels_used))
@@ -102,25 +157,76 @@ async def on_message(message: discord.Message):
 @bot.tree.command(name='setlogchannel', description='Set the channel for bot logs')
 @app_commands.checks.has_permissions(administrator=True)
 async def setLogChannel(interaction: discord.Interaction, channel: discord.TextChannel):
-    global LOG_CHANNEL
-    LOG_CHANNEL = channel.id
+    runtime_settings[interaction.guild.id]['LOGGING_CHANNEL'] = channel.id
+
+    await bot.db.execute("""
+        INSERT INTO SETTINGS (guild_id, log_channel)
+        VALUES ($2, $1)
+        ON CONFLICT (guild_id)
+        DO UPDATE SET log_channel = $1
+    """, channel.id, interaction.guild.id)
+
     await interaction.response.send_message(f'Log channel set to {channel.mention}')
 
 
 @bot.tree.command(name='settimeoutduration', description='Set the timeout duration in days')
 @app_commands.checks.has_permissions(administrator=True)
 async def setTimeoutDuration(interaction: discord.Interaction, duration: float):
-    global TIMEOUT_DURATION
-    TIMEOUT_DURATION = duration
+    runtime_settings[interaction.guild.id]['TIMEOUT_DURATION'] = duration
+
+    await bot.db.execute("""
+        INSERT INTO SETTINGS (guild_id, timeout_dur)
+        VALUES ($2, $1)
+        ON CONFLICT (guild_id)
+        DO UPDATE SET timeout_dur = $1
+    """, duration, interaction.guild.id)
+
     await interaction.response.send_message(f'Timeout duration set to {format_duration(duration)}')
 
 
 @bot.tree.command(name='setdetectiontime', description='Set the amount of time between messages to flag a user in seconds')
 @app_commands.checks.has_permissions(administrator=True)
 async def setDetectionTime(interaction: discord.Interaction, duration: float):
-    global TIME_WINDOW
-    TIME_WINDOW = duration
+    runtime_settings[interaction.guild.id]['DETECTION_WINDOW'] = duration
+
+    await bot.db.execute("""
+        INSERT INTO settings (guild_id, det_win)
+        VALUES ($2, $1)
+        ON CONFLICT (guild_id)
+        DO UPDATE SET det_win = $1
+    """, duration, interaction.guild.id)
+
     await interaction.response.send_message(f'Detection time set to {duration} seconds')
+
+
+@bot.tree.command(name='setchannelthreshold', description='Set the number of channels an account must send the same message to to get flagged')
+@app_commands.checks.has_permissions(administrator=True)
+async def setChannelThreshold(interaction: discord.Interaction, n: int):
+    runtime_settings[interaction.guild.id]['CHANNEL_THRESHOLD'] = n
+
+    await bot.db.execute("""
+        INSERT INTO settings (guild_id, chan_thres)
+        VALUES ($2, $1)
+        ON CONFLICT (guild_id)
+        DO UPDATE SET chan_thres = $1
+    """, n, interaction.guild.id)
+
+    await interaction.response.send_message(f'Channel threshold set to {n}')
+
+
+@bot.tree.command(name='setactiontype', description='Set the action to be taken for flagged accounts')
+@app_commands.checks.has_permissions(administrator=True)
+async def setActionType(interaction: discord.Interaction, type: ActionType):
+    runtime_settings[interaction.guild.id]['ACTION_TYPE'] = n
+
+    await bot.db.execute("""
+        INSERT INTO settings (guild_id, action_type)
+        VALUES ($2, $1)
+        ON CONFLICT (guild_id)
+        DO UPDATE SET action_type = $1
+    """, type.value, interaction.guild.id)
+
+    await interaction.response.send_message(f'Set action type to {type.name}')
 
 
 @bot.tree.command(name='sync', description='Sync commands')
@@ -139,10 +245,17 @@ async def on_command_error(ctx, error):
 
 @bot.event
 async def on_ready():
-    if not check_cache.is_running():
-        check_cache.start()
+    await initialize_db()
 
-    print(f"Logged in as: {bot.user}, Status: Ready!")
+    for guild in bot.guilds:
+        settings = await retrieve_settings(guild.id)
+        if settings:
+            runtime_settings[guild.id] = settings
+
+    if not clear_cache.is_running():
+        clear_cache.start()
+
+    print("Aegis is ready!")
 
 
 bot.run(os.getenv("AEGIS_TOKEN"))
